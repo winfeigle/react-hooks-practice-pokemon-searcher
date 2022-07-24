3fed7d18f6ba1e8050355446ac8363b7b85df3e0
Win Feigle <win.feigle@gmail.com>
2022-07-24 17:36:55 -0600

    Add flip card functionality

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "semantic-ui-react";
 
 function PokemonCard({ poke }) {
+  const [ sprite, setSprite ] = useState(true);
   const { id, name, hp, sprites } = poke;
+
+  const handleCardClick = () => {
+    setSprite(!sprite);
+  }
 
   return (
     <Card>
-      <div>
+      <div onClick={handleCardClick}>
         <div className="image">
-          <img src={sprites.front} alt={name} />
+          <img src={sprite ? sprites.front : sprites.back} alt={name} />
         </div>
         <div className="content">
           <div className="header">{name}</div>
