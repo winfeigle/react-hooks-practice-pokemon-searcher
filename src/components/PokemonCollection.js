4d327b0bc5822cab09fc9d56633ab4287d222ebf
Win Feigle <win.feigle@gmail.com>
2022-07-24 17:31:11 -0600

    render all pokemon from db.json

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,14 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({ pokemon }) {
+  const renderCollection = pokemon.map(poke => {
+    return <PokemonCard key={poke.id} poke={poke}/>
+  })
+
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      { renderCollection }
     </Card.Group>
   );
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/pokemon')
+      .then(res => res.json())
+      .then(setPokemon)
+  },[])
+
+
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
@@ -13,7 +23,7 @@ function PokemonPage() {
       <br />
       <Search />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemon={pokemon}/>
     </Container>
   );
 }
